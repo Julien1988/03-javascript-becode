80d3abe5e8623061f417bc609cb578e0803a5f83
Julien <julienbroens@gmail.com>
2020-01-29 09:45:28 +0100

    exo 05 - 09 check

--- 03-javascript/05-arrays/09-rand-array-stats/script.js
+++ 03-javascript/05-arrays/09-rand-array-stats/script.js
@@ -13,43 +13,65 @@
     // your code here
     let numberArray;
     let addition = 0;
-    let = moyenneNumber = 0;
+    let moyenneNumber = 0;
+    let min = 100;
+    let max = 0;
+    let i = 1;
     
+    // Fonction de création d'un nombre random
 
     function randomizeArray() {
         numberArray = Array.from(new Array(10).keys(), () => Math.floor(Math.random() * (100 - 1) + 1))
-        console.log(numberArray);
         return numberArray;
     }
 
+    // Fonction de sélection du nombre le plus grand
+
     function bigestNumber(arrayOfNumbers){
-        let max=0;
+        
         for (i=0; i<arrayOfNumbers.length; i++) {
             if(arrayOfNumbers[i]>max) max=arrayOfNumbers[i];
         }
-        return console.log(max);
+        return max;
     }
 
+    // Fonction de sélection du nombre le plus petit
+
     function lowestNumber(arrayOfNumbers){
-        let min=100;
+        
         for (i=0; i<arrayOfNumbers.length; i++) {
             if(arrayOfNumbers[i]<min) min=arrayOfNumbers[i];
         }
-        return console.log(min);
+        return  min;
     }
+
+    // Fonction de la somme des nombres
 
     function sommeOfNumbers(arrayOfNumbers) {
         for (i=0; i<arrayOfNumbers.length; i++) {
             addition += arrayOfNumbers[i];
         }
-        return console.log(addition);
+        return addition;
     }
 
+    // Fonction de calcule de la moyenne des nombres
+
     function middleOfNumbers(arrayOfNumbers) {
+        let sumOfNumbers = 0;
         for (i=0; i<arrayOfNumbers.length; i++) {
-            moyenneNumber += arrayOfNumbers[i];
+
+            sumOfNumbers += arrayOfNumbers[i];
+            moyenneNumber = sumOfNumbers / i;
         }
-        return console.log(moyenneNumber / i);
+        return moyenneNumber;
+    }
+
+    function displayNumbers() {
+        for (i = 1; i < 11; i++) {
+
+            document.getElementById("n-" + i).innerHTML = numberArray[i-1]
+        }
+
     }
 
 
@@ -60,8 +82,11 @@
         lowestNumber(numberArray);
         sommeOfNumbers(numberArray);
         middleOfNumbers(numberArray);
-
-        document.getElementById("n-1").innerHTML = numberArray[0];
+        displayNumbers()
+        document.getElementById("min").innerHTML = min;
+        document.getElementById("max").innerHTML = max;
+        document.getElementById("sum").innerHTML = addition;
+        document.getElementById("average").innerHTML = moyenneNumber;
         
         
     });
